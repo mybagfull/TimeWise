91ca0c21f236969b410b614446ba6332b83148a2
mybagfull <baezgamer01@gmail.com>
2023-12-10 20:27:27 +0300

    Diagrams added. Gradle settings.

---
+++ app/src/main/java/com/denishrynkevich/timewise/navigation/NotesNavHost.kt
@@ -0,0 +1,19 @@
+package com.denishrynkevich.timewise.navigation
+
+import androidx.compose.runtime.Composable
+
+sealed class NavRoute(val route: String) {
+    object Start: NavRoute("start_screen")
+    object Main: NavRoute("main_screen")
+    object Add: NavRoute("add_screen")
+    object Node: NavRoute("note_screen")
+}
+
+@Composable
+fun NotesNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavRoute.Start.route) {
+        composable(NavRoute.route) {}
+    }
+}
---
+++ app/src/main/java/com/denishrynkevich/timewise/screens/Start.kt
@@ -0,0 +1,6 @@
+package com.denishrynkevich.timewise.screens
+
+
+@Composable
+fun Start(navHostController: NavHostController) {
+}
